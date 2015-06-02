990e4fd1c3ad3512a140cdf4b2d4308df992f456
Tioben Neenot <tioben.neenot@laposte.net>
2015-06-02 22:38:59 +0200

    Add new Counter class and its unit tests.

---
+++ src/org/hlib4j/math/Counter.java
@@ -0,0 +1,107 @@
+package org.hlib4j.math;
+
+/**
+ * Class to perform a counter according to limits defined thru constructors.
+ *
+ * @author Tioben Neenot
+ */
+public class Counter extends Range<Integer> {
+
+    private int counterStep = 1;
+
+    /**
+     * Builds an instance of the Counter by defining the counter limits and its specific default value.
+     *
+     * @param lowLimit     Low limit for this counter.
+     * @param highLimit    High limit for this counter.
+     * @param defaultValue Default value for this counter.
+     * @throws RangeException If counter is not valid due to its parameters.
+     */
+    public Counter(Integer lowLimit, Integer highLimit, Integer defaultValue) throws RangeException {
+        super(LimitType.BOTH_CLOSE, lowLimit, highLimit, defaultValue);
+    }
+
+    /**
+     * Builds an instance of the Counter by defining the counter limits.
+     *
+     * @param lowLimit  Low limit for this counter.
+     * @param highLimit High limit for this Counter.
+     * @throws RangeException If counter parameters are not valid
+     */
+    public Counter(Integer lowLimit, Integer highLimit) throws RangeException {
+        super(LimitType.BOTH_CLOSE, lowLimit, highLimit);
+    }
+
+    /**
+     * Increments the counter internal value by step value defined with {@link #setCounterStep(int)}.
+     *
+     * @return The new value after increment.
+     * @throws RangeException If the value is reaching the counter upper limit.
+     */
+    int increment() throws RangeException {
+        return incrementByStep(1);
+    }
+
+    /**
+     * Increments the counter internal value by the specific step. This step can not be correspond to the last one
+     * defined with {@link #setCounterStep(int)}.
+     *
+     * @param step Incrementation step value.
+     * @return The new value after increment.
+     * @throws RangeException If the value is reaching the counter upper limit.
+     */
+    int incrementByStep(int step) throws RangeException {
+        this.setCurrentValue(this.getCurrentValue() + step);
+
+        return this.getCurrentValue();
+    }
+
+    /**
+     * Decrements the counter internal value by step value defined with {@link #setCounterStep(int)}.
+     *
+     * @return The new value after decrement.
+     * @throws RangeException If the value is reaching the counter lower limit.
+     */
+    int decrement() throws RangeException {
+        return decrementByStep(1);
+    }
+
+    /**
+     * Decrements the counter internal value by the specific step. This step can not be correspond to the last one
+     * definied with {@link #setCounterStep(int)}.
+     *
+     * @param step Decrementation step value.
+     * @return The new value after decrement.
+     * @throws RangeException If the value is reaching the counter low limit.
+     */
+    int decrementByStep(int step) throws RangeException {
+        this.setCurrentValue(this.getCurrentValue() - step);
+
+        return this.getCurrentValue();
+    }
+
+    /**
+     * Gets the value of the counter step.
+     *
+     * @return Value of the counter step.
+     */
+    int getCounterStep() {
+        return counterStep;
+    }
+
+    /**
+     * Defines the counter step for this counter.
+     *
+     * @param counterStep Counter step value (can be negative).
+     * @throws RangeException If the counterStep value overload one of the counter limits.
+     */
+    void setCounterStep(int counterStep) throws RangeException {
+        int _hypothetic_value = this.getCurrentValue() + counterStep;
+
+        if (_hypothetic_value >= this.getUpperLimitValue() || _hypothetic_value <= this.getLowerLimitValue()) {
+            throw new RangeException("Impossible to set counter step value that overload counter limits");
+        }
+
+        this.counterStep = counterStep;
+    }
+}
---
+++ test/org/hlib4j/math/CounterTest.java
@@ -0,0 +1,103 @@
+package org.hlib4j.math;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link Counter} class.
+ */
+public class CounterTest {
+
+    private Counter counterTesting;
+    private Integer initialValue;
+
+    @Before
+    public void setUp() throws Exception {
+        counterTesting = new Counter(0, 10);
+        initialValue = this.counterTesting.getCurrentValue();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        initialValue = null;
+        counterTesting = null;
+    }
+
+    @Test
+    public void test_Increment_ValidValue_ValueUpdated() throws Exception {
+        Assert.assertEquals(initialValue + 1, counterTesting.increment());
+    }
+
+    @Test(expected = RangeException.class)
+    public void test_Increment_MoreThanUpperLimit_RangeException() throws RangeException {
+        // Setup
+        Counter _local_counter = new Counter(1, 1, 1);
+
+        // SUT
+        _local_counter.increment();
+    }
+
+    @Test
+    public void test_IncrementByStep_ValidValue_ValueUpdated() throws Exception {
+        Assert.assertEquals(initialValue + 5, counterTesting.incrementByStep(5));
+    }
+
+    @Test(expected = RangeException.class)
+    public void test_IncrementByStep_MoreThanUpperLimit_RangeException() throws RangeException {
+        counterTesting.incrementByStep(counterTesting.getUpperLimitValue() + 1);
+    }
+
+    @Test
+    public void test_Decrement_ValidValue_ValueUpdated() throws Exception {
+        // Setup
+        Counter _local_counter = new Counter(-5, 5, 0);
+
+        // Assert
+        Assert.assertEquals(-1, _local_counter.decrement());
+    }
+
+    @Test(expected = RangeException.class)
+    public void test_Decrement_LessThanLowerLimit_RangeException() throws RangeException {
+        this.counterTesting.decrement();
+    }
+
+    @Test
+    public void test_DecrementByStep_ValidValue_ValueUpdated() throws Exception {
+        // Setup
+        Counter _local_counter = new Counter(-5, 5, 0);
+
+        // Assert
+        Assert.assertEquals(-2, _local_counter.decrementByStep(2));
+    }
+
+    @Test
+    public void test_SetCounterStep_SetNewValue_ValueUpdated() throws Exception {
+        this.counterTesting.setCounterStep(5);
+        Assert.assertEquals(5, this.counterTesting.getCounterStep());
+    }
+
+    @Test
+    public void test_GetCounterStep_DefaultValue_AwaitingDefaultStep() throws Exception {
+        Assert.assertEquals(1, this.counterTesting.getCounterStep());
+    }
+
+    @Test(expected = RangeException.class)
+    public void test_SetCounterStep_ValueMoreThanAvailableLimit_RangeException() throws RangeException {
+        this.counterTesting.setCounterStep(this.counterTesting.getUpperLimitValue() + 1);
+    }
+
+    @Test(expected = RangeException.class)
+    public void test_SetCounterStep_NotEnoughValue_RangeException() throws RangeException {
+        // Setup
+        Counter _local_counter = new Counter(0, 4);
+
+        // SUT
+        int _value = _local_counter.increment();
+
+        // Inner assert
+        _local_counter.setCounterStep(3);
+    }
+}
