f7146fcdc7382f32ca846385044b6bdfbf8feac3
Tioben Neenot <tioben.neenot@laposte.net>
2017-05-23 22:45:59 +0200

    Copyrights update

--- src/org/hlib4j/io/process/ProcessOutputStreamFactory.java
+++ src/org/hlib4j/io/process/ProcessOutputStreamFactory.java
@@ -1,3 +1,11 @@
+/*
+ *  Hephaistos 4 Java library: a library with facilities to get more concise code.
+ *
+ *  Copyright (C) 2017 Tioben Neenot
+ *
+ *  This source is distributed under conditions defined into the LICENSE file.
+ */
+
 package org.hlib4j.io.process;
 
 import org.hlib4j.concept.Rule;
